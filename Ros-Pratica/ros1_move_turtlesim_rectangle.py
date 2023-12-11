cf8b0575de56a578c8e1127109fc76e85f3a1b8c
fabiodemo <faberdemo@gmail.com>
2023-12-11 12:00:43 -0300

    Correção de erros

--- Ros-Pratica/ros1_move_turtlesim_rectangle.py
+++ Ros-Pratica/ros1_move_turtlesim_rectangle.py
@@ -3,22 +3,13 @@ from geometry_msgs.msg import Twist
 import sys
 
 
-def turtle_circle(radius):
+def turtle_circle(side_length, speed):
     rospy.init_node('turtlesim', anonymous=True)
     pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
 
-    rate = rospy.Rate(10)
-    vel = Twist()
-    while not rospy.is_shutdown():
-        vel.linear.x = radius
-        vel.linear.y = 0
-        vel.linear.z = 0
-        vel.angular.x = 0
-        vel.angular.y = 0
-        vel.angular.z = 1
-        rospy.loginfo(f"radius = {radius}")
-        pub.publish(vel)
-        rate.sleep()
+    move_cmd = Twist()
+    move_cmd.linear.x = speed
+    move_cmd.angular.z = 0
 
 if __name__ == '__main__':
     try:
